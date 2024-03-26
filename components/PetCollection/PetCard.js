9638d92a760535470348da30cbbcd3fb26e3c50a
Markus <markus.sibbe@t-online.de>
2024-03-26 12:34:35 +0100

    change deletemode

--- components/PetCollection/PetCard.js
+++ components/PetCollection/PetCard.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import Link from "next/link";
 import styled from "styled-components";
+import trashcanIcon from "/public/assets/icons/round_delete_outline_black.png";
 
 const StyledPetCard = styled.button`
   border: solid 2px ${({ deleteMode }) => (deleteMode ? "red" : "black")};
@@ -24,8 +24,11 @@ const StyledName = styled.p`
   margin: 0;
 `;
 
-const StyledPetCardLink = styled(Link)`
-  text-decoration: none;
+const StyledIcon = styled(Image)`
+  position: absolute;
+  top: 0;
+  width: 70%;
+  height: 70%;
 `;
 
 export default function PetCard({ myPet, deleteMode, handleClickOnPetCard }) {
@@ -37,6 +40,9 @@ export default function PetCard({ myPet, deleteMode, handleClickOnPetCard }) {
         }}
         deleteMode={deleteMode}
       >
+        {deleteMode && (
+          <StyledIcon src={trashcanIcon} alt="Delete" height={70} width={70} />
+        )}
         <Image src={myPet.image} alt={myPet.type} height={50} width={50} />
         <StyledName>{myPet.name}</StyledName>
       </StyledPetCard>
