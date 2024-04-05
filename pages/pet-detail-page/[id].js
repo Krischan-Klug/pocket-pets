804ee3f05da6b15cfc8c19d130d2d8f9267367bd
Markus <markus.sibbe@t-online.de>
2024-04-05 14:27:25 +0200

    new folder structure

--- pages/pet-detail-page/[id].js
+++ pages/pet-detail-page/[id].js
@@ -346,7 +346,7 @@ export default function PetDetailPage({
           </StyledButton>
         )}
         <StyledButton
-          onClick={() => router.push(`/${id}/minigames/merge-pets`)}
+          onClick={() => router.push(`/${id}/minigames/merge-pets/`)}
         >
           Minigames
         </StyledButton>
