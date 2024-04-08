ae3bc62c6811fbd60d2610cc6fb64a6b1e1f9f62
Markus <markus.sibbe@t-online.de>
2024-04-08 17:37:50 +0200

    initial commit

--- components/DetailPage/StatusBar.js
+++ components/DetailPage/StatusBar.js
@@ -2,34 +2,24 @@ import styled from "styled-components";
 
 const StyledStatusBarContainer = styled.div`
   background-color: lightgrey;
-  border-radius: 12.5px;
-  height: 25px;
+  border-radius: 10px;
+  height: 20px;
   width: 80%;
   display: flex;
   align-items: center;
   margin: 5px;
 `;
 
-const colors = [
-  "#00FF00", //Green
-  "#33FF00",
-  "#66FF00",
-  "#99FF00",
-  "#CCFF00",
-  "#FFFF00", //Yellow
-  "#FFCC00",
-  "#FF9900",
-  "#FF6600",
-  "#FF3300", //Red
-];
+// Interpolating hue from 0 (red) to 120 (green)
+const interpolateColor = (value) => {
+  const hue = (120 * value) / 100;
+  return `hsl(${hue}, 100%, 50%)`;
+};
 
 const StyledStatusBar = styled.p`
-  background-color: ${(props) => {
-    const index = Math.floor((100 - props.$value) / 10);
-    return colors[index];
-  }};
+  background-color: ${(props) => interpolateColor(props.$value)};
   color: black;
-  border-radius: 12.5px;
+  border-radius: 10px;
   width: ${(props) => props.$value}%;
   padding-left: 20px;
   height: 100%;
