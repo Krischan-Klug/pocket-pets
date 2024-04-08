03008309e7042dfb59cecad5718b0d49a42356ef
Markus <markus.sibbe@t-online.de>
2024-04-08 16:51:04 +0200

    fix problem

--- components/PetCollection/PetCard.js
+++ components/PetCollection/PetCard.js
@@ -4,7 +4,7 @@ import trashcanIcon from "/public/assets/icons/round_delete_outline_black.png";
 import graveImage from "/public/assets/images/grave.png";
 
 const StyledPetCard = styled.button`
-  border: solid 2px ${({ deleteMode }) => (deleteMode ? "red" : "black")};
+  border: solid 2px ${({ $color }) => $color};
   border-radius: 10px;
   height: 110px;
   width: 110px;
@@ -41,7 +41,7 @@ export default function PetCard({ myPet, deleteMode, handleClickOnPetCard }) {
         onClick={() => {
           handleClickOnPetCard(myPet.id);
         }}
-        deleteMode={deleteMode}
+        $color={deleteMode ? "red" : "black"}
       >
         {deleteMode && (
           <StyledDeleteIcon
