f0a8a067bb0c2de5979ed39a5d65500f48c9c205
Markus <markus.sibbe@t-online.de>
2024-04-10 14:19:54 +0200

    The shopTable adapted to generic behavior

--- components/Shop/ShopTable.js
+++ components/Shop/ShopTable.js
@@ -1,5 +1,6 @@
 import MoneyImage from "../util/MoneyImage";
 import HungerImage from "../util/HungerImage";
+import ToyImage from "../util/ToyImage";
 import Image from "next/image";
 import styled from "styled-components";
 
@@ -18,29 +19,56 @@ const StyledImageContainer = styled.div`
   gap: 5px;
 `;
 
+const StyledShopTableWrapper = styled.section`
+  height: calc(100vh - 120px);
+  overflow-y: auto;
+  padding-bottom: 60px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
 export default function ShopTable({ data, onItemClick, category }) {
+  let typeIcon;
+
+  switch (category) {
+    case "food":
+      typeIcon = <HungerImage />;
+      break;
+    case "toy":
+      typeIcon = <ToyImage />;
+      break;
+  }
+
   return (
-    <table>
-      <tbody>
-        {data.map((item) => (
-          <tr key={item.id} onClick={() => onItemClick(item.id, item.cost)}>
-            <StyledTd width={60}>
-              <Image src={item.image} alt={item.name} width={50} height={50} />
-            </StyledTd>
-            <StyledTd width={120}>{item.name}</StyledTd>
-            <StyledTd width={60}>
-              <StyledImageContainer>
-                {item.hunger} <HungerImage />
-              </StyledImageContainer>
-            </StyledTd>
-            <StyledTd width={60}>
-              <StyledImageContainer>
-                {item.cost} <MoneyImage />
-              </StyledImageContainer>
-            </StyledTd>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <StyledShopTableWrapper>
+      <table>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id} onClick={() => onItemClick(item.id, item.cost)}>
+              <StyledTd width={60}>
+                <Image
+                  src={item.image}
+                  alt={item.name}
+                  width={50}
+                  height={50}
+                />
+              </StyledTd>
+              <StyledTd width={120}>{item.name}</StyledTd>
+              <StyledTd width={60}>
+                <StyledImageContainer>
+                  {item.value} {typeIcon}
+                </StyledImageContainer>
+              </StyledTd>
+              <StyledTd width={60}>
+                <StyledImageContainer>
+                  {item.cost} <MoneyImage />
+                </StyledImageContainer>
+              </StyledTd>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </StyledShopTableWrapper>
   );
 }
