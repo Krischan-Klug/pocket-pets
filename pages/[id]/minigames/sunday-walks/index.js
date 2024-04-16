7cef0600474b1bfdb431aaec36863a4b166db65c
Nina Weber <weber_nina@web.de>
2024-04-16 12:09:23 +0200

    add gameOverPopUp, startPopUp, change into treasure hunt

--- pages/[id]/minigames/sunday-walks/index.js
+++ pages/[id]/minigames/sunday-walks/index.js
@@ -1,39 +1,53 @@
 import { useState, useRef, useEffect } from "react";
 import { useRouter } from "next/router";
+import ConfirmationPopup from "@/components/util/ConfirmPopUp";
 // import Image from "next/image";
 import StyledLink from "@/components/StyledComponents/StyledLink";
-import StyledButton from "@/components/StyledComponents/StyledButton";
 import styled from "styled-components";
+import MoneyImage from "@/components/util/MoneyImage";
 
-const gameScreenSize = [400, 400];
+const gameScreenSize = [360, 360];
+const scale = 18; // Pixel width & height of each pet
 
 const StyledGameScreen = styled.canvas`
-  /* background-color: rgb(159, 197, 98); */
   background-color: rgb(134, 189, 61);
   border: 4px solid rgb(194, 140, 90);
   width: ${gameScreenSize[0]}px;
   height: ${gameScreenSize[1]}px;
+
+  /* @media (min-width: 600px) {
+    grid-template-columns: ;
+    grid-template-columns: repeat(scale, 1fr);
+    grid-template-rows: repeat(scale, 1fr);
+  } */
+`;
+
+const StyledMain = styled.main`
+  overflow: hidden;
+`;
+
+const StyledEarnedCoins = styled.p`
+  margin: 0;
 `;
 
 const StyledMobileControlsContainer = styled.section`
-  padding: 0%;
+  padding: 5px;
 `;
 
 const ControlButton = styled.button`
-  /* background-color: var(--accent-color); */
   color: var(--button-text-color);
   background-color: var(--secondary-color);
   border-radius: var(--border-radius);
   border: none;
-  padding: 8px;
+  padding: 5px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  height: 50px;
+  height: 35px;
   width: 70px;
-  font-size: 20px;
+  font-size: 18px;
 `;
 
 const ButtonContainer = styled.div`
@@ -41,12 +55,6 @@ const ButtonContainer = styled.div`
   justify-content: center;
   padding: 5px;
   gap: 20px;
-`;
-
-const StyledButtonContainer2 = styled.div`
-  display: flex;
-  align-content: center;
-  gap: 60px;
 `;
 
 // object of pet images to use for random new petFriend
@@ -62,12 +70,12 @@ const petImages = {
   9: "/assets/images/pets/elephant.png",
   10: "/assets/images/pets/whale.png",
   11: "/assets/images/pets/dragon.png",
+  100: "/assets/images/dollar.png",
 };
 
-export default function SundayWalks({ myPets }) {
+export default function SundayWalks({ onAddMoney, myPets }) {
   const petStart = [[12, 16]];
   const petFriendStart = [12, 7];
-  const scale = 20; // Pixel width & height of each pet
   const defaultSpeed = 500;
   const defaultDirections = {
     38: [0, -1], // up -> not moving on the x-axis but 1 up on the y-axis
@@ -80,8 +88,28 @@ export default function SundayWalks({ myPets }) {
   const [friend, setFriend] = useState(petFriendStart);
   const [direction, setDirection] = useState([0, -1]); // first move of currentPet is UP
   const [speed, setSpeed] = useState(null); // pet does not move before the Start button is clicked
+  const [coins, setCoins] = useState(0);
   const [gameOver, setGameOver] = useState(false);
   const [showMobileControls, setShowMobileControls] = useState(false);
+  const [startPopUpContent, setStartPopUpContent] = useState({
+    message:
+      "The aim of the game is to meet as many of your pet friends and family members to enjoy a nice day in the park. But be careful that nobody harms himself/herself on the park fences or stumbles over a beloved one. To do this, either press the arrow keys or swipe across the display. Have a nice stroll!",
+    onConfirm: () => {
+      setStartPopUpContent({ ...startPopUpContent, show: false });
+      startGame();
+    },
+    onCancel: null,
+    show: true,
+    confirmText: "Start Game",
+  });
+  const [endPopUpContent, setEndPopUpContent] = useState({
+    message: `GAME OVER! Congrats, you won ${coins}!`,
+    onConfirm: () => {
+      onAddMoney(coins);
+      router.push(`/pet-detail-page/${id}`);
+    },
+    show: true,
+  });
 
   const canvasRef = useRef();
   const router = useRouter();
@@ -171,6 +199,7 @@ export default function SundayWalks({ myPets }) {
         newFriend = createFriend();
       }
       setFriend(newFriend);
+      setCoins(coins + 5); // Add coins
       return true;
     }
     return false;
@@ -216,7 +245,7 @@ export default function SundayWalks({ myPets }) {
     petImage.onload = () => {
       context.drawImage(petImage, pet[0] * 1, pet[1] * 1, 1, 1);
     };
-    petImage.src = currentPet.image;
+    // petImage.src = currentPet.image;
     context.fillStyle = "pink"; // pet appearance on gameScreen
     pet.forEach(([x, y]) => context.fillRect(x, y, 1, 1));
 
@@ -225,28 +254,29 @@ export default function SundayWalks({ myPets }) {
     friendImage.onload = () => {
       context.drawImage(friendImage, friend[0] * 1, friend[1] * 1, 1, 1);
     };
-    const randomIndex =
-      Math.floor(Math.random() * Object.keys(petImages).length) + 1;
-    friendImage.src = petImages[randomIndex];
+    // const randomIndex =
+    //   Math.floor(Math.random() * Object.keys(petImages).length) + 1;
+    // friendImage.src = petImages[randomIndex];
+    friendImage.src = petImages[100];
     context.fillStyle = "rgb(134, 189, 61)";
     context.fillRect(friend[0], friend[1], 1, 1);
-  }, [pet, friend, gameOver]);
+  }, [pet, friend, gameOver, id, myPets]);
 
   return (
     <div role="button" tabIndex="0" onKeyDown={(e) => movePet(e)}>
       <header>
         <StyledLink href={`/pet-detail-page/${id}`}>Back</StyledLink>
-        <h1>Sunday Walks</h1>
+        <h1>Treasure Hunt</h1>
+        <StyledEarnedCoins>
+          Win: {coins} <MoneyImage />
+        </StyledEarnedCoins>
       </header>
-      <main>
-        <p>{/* Current highscore {points} / Current earned {coins} */}</p>
+      <StyledMain>
         <StyledGameScreen
           ref={canvasRef}
           width={gameScreenSize[0]}
           height={gameScreenSize[1]}
         />
-        {gameOver && <div>GAME OVER!</div>}
-        <StyledButton onClick={startGame}>Start Game</StyledButton>
         {showMobileControls && (
           <StyledMobileControlsContainer>
             <ButtonContainer>
@@ -281,7 +311,23 @@ export default function SundayWalks({ myPets }) {
             </ButtonContainer>
           </StyledMobileControlsContainer>
         )}
-      </main>
+        {/* { gameOver && <div>GAME OVER!</div>} */}
+        {/* <StyledButton onClick={startGame}>Start Game</StyledButton> */}
+        {startPopUpContent.show && (
+          <ConfirmationPopup
+            message={startPopUpContent.message}
+            onConfirm={startPopUpContent.onConfirm}
+            onCancel={startPopUpContent.onCancel}
+            confirmText={startPopUpContent.confirmText}
+          />
+        )}
+        {gameOver && endPopUpContent.show && (
+          <ConfirmationPopup
+            message={endPopUpContent.message}
+            onConfirm={endPopUpContent.onConfirm}
+          />
+        )}
+      </StyledMain>
     </div>
   );
 }
