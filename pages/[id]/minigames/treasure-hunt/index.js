47cd972552c4db5a19ae70a034e5bf965adef6c4
Nina Weber <weber_nina@web.de>
2024-04-17 16:44:52 +0200

    add PR feedback

--- pages/[id]/minigames/treasure-hunt/index.js
+++ pages/[id]/minigames/treasure-hunt/index.js
@@ -34,7 +34,6 @@ const ControlButton = styled.button`
   padding: 5px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   display: flex;
-  flex-direction: row;
   align-items: center;
   justify-content: center;
   height: 35px;
