0c95dba8b850f73b0d850cc2ed415bcf6d88cf03
Markus <markus.sibbe@t-online.de>
2024-04-19 16:51:02 +0200

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,5 @@
 import GlobalStyle from "../styles";
-import { useEffect, useState } from "react";
-import useLocalStorageState from "use-local-storage-state";
 import AudioInterface from "@/components/AudioPlayer/AudioInterface.js";
-import SettingPopUp from "@/components/SettingPage/SettingPopUp";
-import SettingPageButton from "@/components/SettingPage/SettingPageButton";
-import { useRouter } from "next/router";
-import { useMoneyStore } from "@/hooks/stores/moneyStore";
-import { usePetStore } from "@/hooks/stores/petStore";
-import { petEvents, userEvents } from "@/lib/events";
-import { useInventoryStore } from "@/hooks/stores/inventoryStore";
-import { useTimeStore } from "@/hooks/stores/timeStore";
 import { SessionProvider } from "next-auth/react";
 import LoginComponent from "@/components/util/LoginComponent";
 import GameSession from "@/components/GameSession";
